ada646cd8f0b0366aa9d6b187b2ed58812cb3435
steven2805 <stevenmac90@googlemail.com>
2017-10-02 15:01:47 +0100

    added some more map rendering details

--- client/src/game_logic/levelBuilder.js
+++ client/src/game_logic/levelBuilder.js
@@ -1,11 +1,26 @@
-
-
-
-
 levelBuilder = function(plan){
-  this.width = plan[0].length;
-  this.height = plan.length;
+  this.width = plan[1].length;
+  this.height = plan[1][0].length;
   this.cubeSize = 40; 
+ 
+ // this is the styling for later on, not sure am actually going to use this
+  this.groundStyle = plan[0][0];
+  this.wallStyle = plan[0][1];
+  this.grassStyle = plan[0][2];
+
+  this.walls =[];
+  this.grass =[];
+  this.brick =[];
+  this.coins =[];
+  this.wood  =[];
+}
+
+
+levelBuilder.prototype.constructMap = function(){
+  var yArray = [];
+  var xArray = [];
+
+  
 }
 
 
--- client/src/game_logic/levelPlans.js
+++ client/src/game_logic/levelPlans.js
@@ -1,4 +1,5 @@
-var levelsPlan = [[["Summer Image","Red Brick","Snow ground",4,5,6,7,8],[
+var levelsPlan = 
+[[["Summer Image","Red Brick","Snow ground",4,5,6,7,8],[
 "                                ", 
 "                                ", 
 "  P     c                       ", 
@@ -17,6 +18,28 @@ var levelsPlan = [[["Summer Image","Red Brick","Snow ground",4,5,6,7,8],[
 "xxx                    gbbbbbbbb", 
 "xxxxxgggggggggg  gggggggbbbbbbbb", 
 "xxxxxxxxxxxxxxx  xxxxxxxxxxxxxxx"  
+]],
+
+[["Golden_Coast","Green leaves","Dirt Ground",4,5,6,7,8],[
+"                                ", 
+"                                ", 
+"  P     c                       ", 
+" xxxx xxxxxxx                   ", 
+"                                ", 
+"             sdasdasdasdas      ", 
+"                                ", 
+"x x x                    ggggg  ", 
+"xxxxx   xxxxxxxxxxxxxx  bbbbbb  ", 
+"xxxxx                   bb  K   ", 
+"xx xx            xxxxxxxbb  bbbb", 
+"xxxxx     c             bb    bb", 
+"xxxxxxxxx xxx              c  bb", 
+"xxxxx         c         bbbbbbbb", 
+"xxxD         xx      x  bbbbbbbb", 
+"xxx                    gbbbbbbbb", 
+"xxxxxgggggggggg  gggggggbbbbbbbb", 
+"xxxxxxxxxxxxxxx  xxxxxxxxxxxxxxx" 
+
 ]]]
 
 module.exports = levelsPlan;
